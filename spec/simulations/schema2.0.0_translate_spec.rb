aa0a339d0e89fd57e8050f6966b7dfe7f4a75da5
corymosiman12 <cory.mosiman12@gmail.com>
2020-03-25 22:25:51 -0600

    update to v2.0 in xsi

--- spec/simulations/schema2.0.0_translate_spec.rb
+++ spec/simulations/schema2.0.0_translate_spec.rb
@@ -5,6 +5,42 @@ require 'openstudio/model_articulation/os_lib_model_generation_bricr'
 require 'buildingsync'
 require 'buildingsync/translator'
 
+describe 'A PROPER schema2.0.0 L000_OpenStudio_Simulation_01.xml' do
+  before(:all) do
+    @f = 'L000_OpenStudio_Simulation_01'
+    @test_file = File.join(File.dirname(__FILE__), "../use_cases/schema2.0.0/examples/#{@f}.xml")
+    @output_path = File.join(File.dirname(__FILE__), 'schema2.0.0', @f)
+    FileUtils.mkdir_p(@output_path) unless File.exist?(@output_path)
+    # TODO: make sure it validates against schema 2.0
+    @translator = BuildingSync::Translator.new(@test_file, @output_path, nil, 'ASHRAE90.1', false)
+    @result_from_write_osm = @translator.write_osm
+    @result_from_write_osw = @translator.write_osws # this just returns the Baseline scenario
+    @output_osm = File.join(@output_path, 'in.osm')
+    @output_osw = File.join(@output_path, 'Baseline', 'in.osw')
+  end
+
+  it "Should create an L000_OpenStudio_Simulation_01/in.osm file" do
+    expect(File.exist?(@output_osm)).to be true
+    puts @result_from_write_osm
+  end
+
+  it "Should have a system_type of 'VAV with reheat'" do
+    expect(@result_from_write_osm["system_type"]).to eq("VAV with reheat")
+  end
+
+  it "Should have a bldg_type of 'LargeOffice'" do
+    expect(@result_from_write_osm["bldg_type"]).to eq("LargeOffice")
+  end
+
+  it "Should have a template of 'DOE Ref Pre-1980'" do
+    expect(@result_from_write_osm["template"]).to eq("DOE Ref Pre-1980")
+  end
+
+  it "Should create an L000_OpenStudio_Simulation_01/Baseline/in.osw file" do
+    expect(File.exist?(@output_osw)).to be true
+  end
+end
+
 describe 'A PROPER schema2.0.0 L000_OpenStudio_Simulation_02.xml' do
   before(:all) do
     @f = 'L000_OpenStudio_Simulation_02'
@@ -38,6 +74,4 @@ describe 'A PROPER schema2.0.0 L000_OpenStudio_Simulation_02.xml' do
   it "Should create an L000_OpenStudio_Simulation_02/Baseline/in.osw file" do
     expect(File.exist?(@output_osw)).to be true
   end
-
 end
-
