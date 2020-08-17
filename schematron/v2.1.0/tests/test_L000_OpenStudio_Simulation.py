c7b8a0922776337dd94230fb66a97e50f41be5be
corymosiman12 <cory.mosiman12@gmail.com>
2020-08-17 17:56:36 -0600

    add golden file 2 for L000 openstudio tests

--- schematron/v2.1.0/tests/test_L000_OpenStudio_Simulation.py
+++ schematron/v2.1.0/tests/test_L000_OpenStudio_Simulation.py
@@ -3,9 +3,10 @@ import os
 from tools.validate_sch import validate_schematron
 from conftest import failures_by_role, AssertFailureRolesMixin, golden_tree, remove_element, SCH_DIR
 
-class TestL000OpenStudioSimulation(AssertFailureRolesMixin):
+class TestL000OpenStudioSimulation01(AssertFailureRolesMixin):
     schematron = os.path.join(SCH_DIR, 'L000_OpenStudio_Simulation.sch')
-    golden_file = os.path.join(SCH_DIR, 'golden_files', 'L000_OpenStudio_Simulation_01.xml')
+    golden_file_name = 'L000_OpenStudio_Simulation_01'
+    golden_file = os.path.join(SCH_DIR, 'golden_files', f"{golden_file_name}.xml")
 
     def test_golden_file_is_valid(self):
         # -- Act
@@ -14,9 +15,9 @@ class TestL000OpenStudioSimulation(AssertFailureRolesMixin):
         # -- Assert
         self.assert_failure_roles(failures, {})
 
-    def test_fails_when_no_address_or_climate_zone(self):
+    def test_fails_when_no_address(self):
         # -- Setup
-        tree = golden_tree('L000_OpenStudio_Simulation_01')
+        tree = golden_tree(self.golden_file_name)
 
         # verify it's valid
         failures = validate_schematron(self.schematron, tree)
@@ -34,7 +35,7 @@ class TestL000OpenStudioSimulation(AssertFailureRolesMixin):
 
     def test_fails_when_package_of_measures_scenario_missing(self):
         # -- Setup
-        tree = golden_tree('L000_OpenStudio_Simulation_01')
+        tree = golden_tree(self.golden_file_name)
 
         # verify it's valid
         failures = validate_schematron(self.schematron, tree)
@@ -52,7 +53,7 @@ class TestL000OpenStudioSimulation(AssertFailureRolesMixin):
 
     def test_fails_when_package_of_measures_scenario_missing_reference_case(self):
         # -- Setup
-        tree = golden_tree('L000_OpenStudio_Simulation_01')
+        tree = golden_tree(self.golden_file_name)
 
         # verify it's valid
         failures = validate_schematron(self.schematron, tree)
@@ -68,3 +69,34 @@ class TestL000OpenStudioSimulation(AssertFailureRolesMixin):
         self.assert_failure_roles(failures, {'ERROR': 1})
         assert failures[
                    0].message == "count(auc:Scenario[@ID='Baseline' and auc:ScenarioType/auc:PackageOfMeasures/auc:ReferenceCase/@IDref='Baseline' and auc:ScenarioName='Baseline']) = 1"
+
+
+class TestL000OpenStudioSimulation02(AssertFailureRolesMixin):
+    schematron = os.path.join(SCH_DIR, 'L000_OpenStudio_Simulation.sch')
+    golden_file_name = 'L000_OpenStudio_Simulation_02'
+    golden_file = os.path.join(SCH_DIR, 'golden_files', f"{golden_file_name}.xml")
+
+    def test_golden_file_is_valid(self):
+        # -- Act
+        failures = validate_schematron(self.schematron, self.golden_file)
+
+        # -- Assert
+        self.assert_failure_roles(failures, {})
+
+    def test_fails_when_no_climate_zone_type(self):
+        # -- Setup
+        tree = golden_tree(self.golden_file_name)
+
+        # verify it's valid
+        failures = validate_schematron(self.schematron, tree)
+        self.assert_failure_roles(failures, {})
+
+        # remove the climate zone
+        tree = remove_element(tree, '//auc:Building/auc:ClimateZoneType')
+
+        # -- Act
+        failures = validate_schematron(self.schematron, tree)
+
+        # -- Assert
+        self.assert_failure_roles(failures, {'ERROR': 1})
+        assert failures[0].message == "(auc:Address/auc:City and auc:Address/auc:State) or auc:ClimateZoneType/auc:ASHRAE/auc:ClimateZone or auc:ClimateZoneType/auc:CaliforniaTitle24/auc:ClimateZone"
