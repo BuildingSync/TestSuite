c82829c61dd4e734e0e1a2162904773da63b48e5
Xiong <jie.xiong@nrel.gov>
2021-03-04 18:53:54 -0700

    bsyncr naming modifications
    Change all BSyncr to bsyncr

--- schematron/v2.2.0/tests/test_v2-2-0_L100_Audit_bsyncr.py
+++ schematron/v2.2.0/tests/test_v2-2-0_L100_Audit_bsyncr.py
@@ -3,10 +3,10 @@ import os
 from testsuite.validate_sch import validate_schematron
 from schematron.conftest import AssertFailureRolesMixin, v2_2_0_SCH_DIR
 
-class TestL100AuditBSyncr(AssertFailureRolesMixin):
+class TestL100Auditbsyncr(AssertFailureRolesMixin):
     schematron1 = os.path.join(v2_2_0_SCH_DIR, 'v2-2-0_L100_Audit.sch')
     schematron2 = os.path.join(v2_2_0_SCH_DIR, 'v2-2-0_bsyncr.sch')
-    exemplary_file = os.path.join(v2_2_0_SCH_DIR, 'exemplary_files', 'L100_Audit_BSyncr.xml')
+    exemplary_file = os.path.join(v2_2_0_SCH_DIR, 'exemplary_files', 'L100_Audit_bsyncr.xml')
 
     def test_exemplary_file_is_valid(self):
         # -- Act
