e3764cb5f55d03898224ce0cf6d4975241ef4cc7
Xiong <jie.xiong@nrel.gov>
2021-03-04 17:52:25 -0700

    Test function for bsyncr
    Create test function for bsyncr use case

---
+++ schematron/v2.2.0/tests/test_v2-2-0_bsyncr.py
@@ -0,0 +1,17 @@
+import os
+
+from testsuite.validate_sch import validate_schematron
+from schematron.conftest import AssertFailureRolesMixin, v2_2_0_SCH_DIR
+
+class Testbsyncr(AssertFailureRolesMixin):
+    schematron = os.path.join(v2_2_0_SCH_DIR, 'v2-2-0_bsyncr.sch')
+    exemplary_file = os.path.join(v2_2_0_SCH_DIR, 'exemplary_files', 'bsyncr.xml')
+
+    def test_exemplary_file_is_valid(self):
+        # -- Act
+        failures = validate_schematron(self.schematron, self.exemplary_file)
+
+        # -- Assert
+        self.assert_failure_messages(failures, {})
+
+ 
