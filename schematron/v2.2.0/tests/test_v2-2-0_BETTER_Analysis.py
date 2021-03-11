b4ab1647b6f739a475dc325599b3b6c34cbd89a2
aviveiro <viveiros.austin@gmail.com>
2021-03-11 10:13:14 -0500

    fix end of file

--- schematron/v2.2.0/tests/test_v2-2-0_BETTER_Analysis.py
+++ schematron/v2.2.0/tests/test_v2-2-0_BETTER_Analysis.py
@@ -3,6 +3,7 @@ import os
 from testsuite.validate_sch import validate_schematron
 from schematron.conftest import AssertFailureRolesMixin, v2_2_0_SCH_DIR
 
+
 class TestBETTERAnalysis(AssertFailureRolesMixin):
     schematron = os.path.join(v2_2_0_SCH_DIR, 'v2-2-0_BETTER.sch')
     exemplary_file = os.path.join(v2_2_0_SCH_DIR, 'exemplary_files', 'BETTER_BuildingSync_Input.xml')
